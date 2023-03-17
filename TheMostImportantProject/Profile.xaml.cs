f022d37b479b52946f853545d7c80db17b463709
Almaz <52231348+Heahdix@users.noreply.github.com>
2023-03-17 15:41:04 +0300

    Добавьте файлы проекта.

---
+++ TheMostImportantProject/Profile.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+
+namespace TheMostImportantProject
+{
+
+    public partial class Profile : Window
+    {
+        public Profile()
+        {
+            InitializeComponent();
+        }
+        private void Label_MouseUpGoToMenu(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            MenuScreen menuScreen = new MenuScreen();
+            menuScreen.Show();
+            this.Close();
+        }
+
+        private void Label_MouseUpGoToBasket(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Basket basket = new Basket();
+            basket.Show();
+            this.Close();
+        }
+
+        private void Label_MouseUpGoToOrders(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            
+        }
+
+      
+
+        
+    }
+}
